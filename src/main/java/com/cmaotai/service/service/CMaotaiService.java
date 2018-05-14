66d7d690c20be3e815a842453a31380f2e86ceb1
袁国华 <yuanguohua@hujiang.com>
2018-05-14 09:58:15 +0800

    动态UA

--- src/main/java/com/cmaotai/service/service/CMaotaiService.java
+++ src/main/java/com/cmaotai/service/service/CMaotaiService.java
@@ -1,7 +1,9 @@
 package com.cmaotai.service.service;
 
+import com.cmaotai.service.address.Address;
 import com.cmaotai.service.model.CMaotaiOrderStatus;
 import com.cmaotai.service.model.CMotaiDefaultAddress;
+import com.google.common.collect.Lists;
 import java.io.UnsupportedEncodingException;
 import java.util.List;
 import org.springframework.http.HttpEntity;
@@ -14,11 +16,13 @@ public interface CMaotaiService {
 
     String CMAOTAI_URL = "https://www.cmaotai.com/API/Servers.ashx?";
 
-    String UA = "Mozilla/5.0 (iPhone; CPU iPhone OS 11_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E216[iphone/1.0.23/6bcfa8eca598d009a9b6d43861783eee/63e60631de817fb5c28e35b95298411a]";
-//    List<String> USERAGENTS = Lists.newArrayList(
-//        "Mozilla/5.0 (iPhone; CPU iPhone OS 11_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E216[iphone/1.0.23/6bcfa8eca598d009a9b6d43861783eee/63e60631de817fb5c28e35b95298411a]",
-//        "Mozilla/5.0 (iPhone; CPU iPhone OS 9_3_5 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Mobile/13G36[iphone/1.0.23/7b114f75dcb98d35c68ccef4007b3210/b079c2e3e943108643ca0387da836b09]");
+    List<String> USERAGENTS = Lists.newArrayList(
+        "Mozilla/5.0 (iPhone; CPU iPhone OS 11_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E216[iphone/1.0.23/6bcfa8eca598d009a9b6d43861783eee/63e60631de817fb5c28e35b95298411a]",
+        "Mozilla/5.0 (iPhone; CPU iPhone OS 9_3_5 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Mobile/13G36[iphone/1.0.23/7b114f75dcb98d35c68ccef4007b3210/b079c2e3e943108643ca0387da836b09]");
 
+    default String getUA() {
+        return USERAGENTS.get(Address.random(USERAGENTS.size() - 1));
+    }
     HttpHeaders parseHeader(HttpHeaders httpHeaders);
 
     boolean signup(String mobile, String pwd) throws Exception;
