fa8ea2177a7c17e94748e4130b966f8702bfc512
袁国华 <yuanguohua@hujiang.com>
2018-05-19 21:16:11 +0800

    页面开启

--- src/main/java/com/cmaotai/service/main/CmaotaiStart.java
+++ src/main/java/com/cmaotai/service/main/CmaotaiStart.java
@@ -1,12 +1,5 @@
 package com.cmaotai.service.main;
 
-import com.cmaotai.service.service.StartService;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class CmaotaiStart {
 
-    public static void main(String[] args) throws Exception {
-        new StartService().start();
-    }
 }
