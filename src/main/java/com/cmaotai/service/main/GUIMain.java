f6661a7b5e1543f90583763685ae99e9b3d3e35e
袁国华 <yuanguohua@hujiang.com>
2018-05-20 13:37:20 +0800

    默认0分钟

--- src/main/java/com/cmaotai/service/main/GUIMain.java
+++ src/main/java/com/cmaotai/service/main/GUIMain.java
@@ -243,7 +243,7 @@ public class GUIMain extends JFrame {
         panel1.add(label4, new GridConstraints(2, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_NONE,
             GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
         timer = new JTextField();
-        timer.setText("2");
+        timer.setText("0");
         panel1.add(timer, new GridConstraints(2, 1, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_HORIZONTAL,
             GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(150, -1), null,
             0, false));
