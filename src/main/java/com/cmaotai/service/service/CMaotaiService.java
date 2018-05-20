e74d01f3e81370bd18a3d00e59359e9b159e1c44
袁国华 <yuanguohua@hujiang.com>
2018-05-20 23:02:50 +0800

    修复不能修改密码的接口

--- src/main/java/com/cmaotai/service/service/CMaotaiService.java
+++ src/main/java/com/cmaotai/service/service/CMaotaiService.java
@@ -14,6 +14,8 @@ public interface CMaotaiService {
 
     String CMAOTAI_URL = "https://www.cmaotai.com/API/Servers.ashx?";
 
+    String CMAOTAI_YSAPP_URL = "https://www.cmaotai.com/YSApp_API/YSAppServer.ashx?";
+
     String UA = "Mozilla/5.0 (iPhone; CPU iPhone OS 11_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E216[iphone/1.0.23/6bcfa8eca598d009a9b6d43861783eee/63e60631de817fb5c28e35b95298411a]";
 //    List<String> USERAGENTS = Lists.newArrayList(
 //        "Mozilla/5.0 (iPhone; CPU iPhone OS 11_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E216[iphone/1.0.23/6bcfa8eca598d009a9b6d43861783eee/63e60631de817fb5c28e35b95298411a]",
@@ -44,6 +46,11 @@ public interface CMaotaiService {
     default ResponseEntity<String> post(String action, HttpHeaders httpHeaders) {
         return restTemplate
             .exchange(CMAOTAI_URL + action, HttpMethod.POST, new HttpEntity<String>(httpHeaders), String.class);
+    }
+
+    default ResponseEntity<String> ysPost(String action, HttpHeaders httpHeaders) {
+        return restTemplate
+            .exchange(CMAOTAI_YSAPP_URL + action, HttpMethod.POST, new HttpEntity<String>(httpHeaders), String.class);
     }
 
     default ResponseEntity<String> get(String action) {
