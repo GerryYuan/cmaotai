de6f975822c3b9c6a924e5a777129ee11cff0333
袁国华 <yuanguohua@hujiang.com>
2018-05-22 12:42:42 +0800

    更改设备号

--- src/main/java/com/cmaotai/service/service/CMaotaiService.java
+++ src/main/java/com/cmaotai/service/service/CMaotaiService.java
@@ -19,13 +19,13 @@ public interface CMaotaiService {
     String CMAOTAI_URL = "https://www.cmaotai.com/API/Servers.ashx?";
     String CMAOTAI_YSAPP_URL = "https://www.cmaotai.com/YSApp_API/YSAppServer.ashx?";
     List<String> USERAGENTS = Lists.newArrayList(
-        "Mozilla/5.0 (iPhone; CPU iPhone OS 11_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E216[iphone/1.0.23/6bcfa8eca598d009a9b6d43861783eee/63e60631de817fb5c28e35b95298411a]",
-        "Mozilla/5.0 (iPhone; CPU iPhone OS 9_3_5 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Mobile/13G36[iphone/1.0.23/7b114f75dcb98d35c68ccef4007b3210/b079c2e3e943108643ca0387da836b09]",
-        "Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_2 like Mac OS X) AppleWebKit/603.2.4 (KHTML, like Gecko) Mobile/14F89[iphone/1.0.23/c1d1f2a823e0064dc865e6a2ad9386e9/60cb8e6580055022ee6d5fad77ab39aa]",
-        "Mozilla/5.0 (iPhone; CPU iPhone OS 11_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E302[iphone/1.0.23/393b7b99466f0a0ceac3aa2e35a73495/dbfbf5530b25d01a432fbaf5f1f708ef]");
+        "Mozilla/5.0 (Linux; Android 5.1; OPPO R9m Build/LMY47I; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/43.0.2357.121 Mobile Safari/537.36[android/1.0.23/35facc15ca64a3bb85c81875cde35d5c/84afe039b858896b6c41e5d811c2e920]");
 
     default String getUA() {
-        return USERAGENTS.get(Address.random(USERAGENTS.size() - 1));
+        if (USERAGENTS.size() == 1) {
+            return USERAGENTS.get(0);
+        }
+        return USERAGENTS.get(Address.random(USERAGENTS.size()));
     }
 
     HttpHeaders parseHeader(HttpHeaders httpHeaders);
